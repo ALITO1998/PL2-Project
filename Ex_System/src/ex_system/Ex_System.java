fcf367039f6c9f6db73e11e27f1ceab1a7cad41c
ALITO1998 <alialaa144144@gmail.com>
2018-12-06 09:46:05 +0200

    Sec merge

--- Ex_System/src/ex_system/Ex_System.java
+++ Ex_System/src/ex_system/Ex_System.java
@@ -5,6 +5,8 @@
  */
 package ex_system;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
@@ -13,7 +15,11 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
@@ -26,9 +32,21 @@ import javafx.stage.Stage;
  * @author Ali
  */
 public class Ex_System extends Application {
-    
+    static String User_Type = new String();
     @Override
     public void start(Stage primaryStage) {
+        /*MenuBar menubar = new MenuBar();
+        Menu File = new Menu("File");
+        MenuItem Exit = new MenuItem("Exit");
+        MenuItem Restore = new MenuItem("Restore");
+        MenuItem Minimize = new MenuItem("Minimize");
+        MenuItem Maximize = new MenuItem("Maximize");
+        File.getItems().addAll(Restore,Minimize,Maximize,Exit);
+        Menu Edit = new Menu("Edit");
+        Menu View = new Menu("View");
+        Menu Help = new Menu("Help");
+        menubar.getMenus().addAll(File,Edit,View,Help);*/
+        Text footer = new Text("Power by : @PL_best_Team");
         Text title = new Text("Examenation System");
         title.setFont(Font.font("Tahoma",FontWeight.BOLD,FontPosture.ITALIC, 20));
         ImageView amg3 = new ImageView("Admin.jpg");
@@ -37,16 +55,53 @@ public class Ex_System extends Application {
         Button btn2 = new Button("", amg3);
         Button btn1 = new Button("", amg2);
         Button btn = new Button("",amg1);
-        btn2.setOnAction((ActionEvent event) -> {
-            System.out.println("I'm a Admin");
+        btn2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    User_Type = "Admin";
+                    primaryStage.close();
+                    Stage secStage = new Stage();
+                    LogIn x = new LogIn();
+                    x.start(secStage);
+                    //System.out.println("I'm a Admin");
+                } catch (Exception ex) {
+                    Logger.getLogger(Ex_System.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
         });
-        btn.setOnAction((ActionEvent event) -> {
-            System.out.println("I'm a Professor");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    User_Type = "Lecturer";
+                    primaryStage.close();
+                    Stage secStage = new Stage();
+                    LogIn x = new LogIn();
+                    x.start(secStage);
+                    //System.out.println("I'm a Professor");
+                } catch (Exception ex) {
+                    Logger.getLogger(Ex_System.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
         });
-        btn1.setOnAction((ActionEvent event) -> {
-            System.out.println("I'm a Student");
+        btn1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    User_Type = "Student";
+                    primaryStage.close();
+                    Stage secStage = new Stage();
+                    LogIn x = new LogIn();
+                    x.start(secStage);
+                    //System.out.println("I'm a Student");
+                } catch (Exception ex) {
+                    Logger.getLogger(Ex_System.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
         });
-        
+        BorderPane root1 = new BorderPane();
         GridPane root = new GridPane();
         root.setAlignment(Pos.CENTER);
         root.setHgap(10);
@@ -56,15 +111,16 @@ public class Ex_System extends Application {
         root.add(btn1,1,1);
         root.add(btn2,2,1);
         root.setPadding(new Insets(25, 25, 25, 25));
-        
-        Scene scene = new Scene(root);
+        root1.setCenter(root);
+        //root1.setTop(menubar);
+        root1.setBottom(footer);
+        Scene scene = new Scene(root1);
         
         primaryStage.setTitle("Examenation System");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
-
     /**
      * @param args the command line arguments
      */
---
+++ Ex_System/src/ex_system/LogIn.java
@@ -0,0 +1,125 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template filemanegar, choose Tools | Templates
+ * and open the template in the editor  .
+ */
+package ex_system;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Ali
+ */
+public class LogIn extends Application{
+
+    @Override
+    public void start(Stage sec) throws Exception {
+         sec.setTitle("Login");
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text scenetitle = new Text("Welcome");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        Label userName = new Label("User ID :");
+        grid.add(userName, 0, 1);
+
+        final TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Label pw = new Label("Password:");
+        grid.add(pw, 0, 2);
+
+        final PasswordField pwBox = new PasswordField();
+        grid.add(pwBox, 1, 2);
+
+        Button btn = new Button("Sign in");
+        HBox hbtn = new HBox();
+        hbtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbtn.getChildren().add(btn);
+        grid.add(hbtn, 1, 4);
+        Button btn1 = new Button("Back");
+        grid.add(btn1, 0, 4);
+        final Text actiontarget = new Text();
+        grid.add(actiontarget, 1, 6);
+
+        
+
+        Scene scene = new Scene(grid, 300, 275);
+        sec.setScene(scene);
+        sec.show();   
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent e) {
+                sec.close();
+                Stage therd = new Stage();
+                switch(Ex_System.User_Type){
+                    case "Lecturer" :
+                        if(true){//new filemanegar().login(userTextField.getText(),pwBox.getText())){
+                            try {
+                                new LecturerPanal().start(therd);
+                            } catch (Exception ex) {
+                                Logger.getLogger(LogIn.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                        }else{
+                            actiontarget.setFill(Color.RED);
+                            actiontarget.setText("Invalid Username and Password!");
+                         }
+                    case "Student" :
+                        if(false){//new filemanegar().login(userTextField.getText(),pwBox.getText())){
+                            
+                    
+                        }else{
+                            actiontarget.setFill(Color.RED);
+                            actiontarget.setText("Invalid Username and Password!");
+                        }
+                    case "Admin" :
+                        if(false){//new filemanegar().login(userTextField.getText(),pwBox.getText())){
+                            
+                    
+                        }else{
+                            actiontarget.setFill(Color.RED);
+                            actiontarget.setText("Invalid Username and Password!");
+                        }
+                  
+                }
+                
+            }
+        });
+        btn1.setOnAction(new EventHandler<ActionEvent>() {
+             @Override
+             public void handle(ActionEvent event) {
+                 sec.close();
+                 Ex_System x = new Ex_System();
+                 Stage pS = new Stage();
+                 x.start(pS);//To change body of generated methods, choose Tools | Templates.
+             }
+         });
+    }
+    }
+    
+
